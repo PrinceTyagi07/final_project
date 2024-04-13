b7f69e694837003f15a9f09eea383eb62682f773
Prnce Tyagi <princetyagi1901@gmail.com>
2024-04-13 20:14:39 +0530

    new

--- src/App.jsx
+++ src/App.jsx
@@ -5,8 +5,10 @@ import Men from './Components/Men'
 import Women from './Components/Women'
 import Beauty from './Components/Beauty'
 import Fashion from './Components/Fashion'
+import Kids from './Components/Kids'
 import Navbar from './Components/Navbar'
-
+import { Navbarlinks } from './Navlinks'
+import { Route,Routes } from 'react-router-dom'
 const App = () => {
   return (
     <div className=''>
