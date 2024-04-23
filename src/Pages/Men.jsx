09002970012ada13c1726d936c1db32eba1f1dff
Prnce Tyagi <princetyagi1901@gmail.com>
2024-04-23 06:10:56 +0530

    up

--- src/Pages/Men.jsx
+++ src/Pages/Men.jsx
@@ -1,12 +1,38 @@
-import React from 'react'
-
+import React, { useRef } from 'react'
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/css';
+import "swiper/css/free-mode";
+import "swiper/css/pagination";
+import "swiper/css/navigation";
+import { Autoplay, FreeMode, Navigation, Pagination } from "swiper/modules";
 const Men = ({setChanges}) => {
   const setchange = setChanges;
   function changehandler() {
     setchange(false);
   }
+  const SlideRef = useRef;
   return (
-    <div onClick={changehandler} className='h-screen'>Men</div>
+    <div onClick={changehandler} className='h-screen'>
+    
+    <Swiper
+        pagination={false}
+        modules={[FreeMode, Pagination, Navigation, Autoplay]}
+        ref={SlideRef}
+        className=" flex items-center justify-center text-center h-full w-full"
+        loop={true}
+        freeMode={true}
+        slidesPerView={1}
+        spaceBetween={50}
+        autoplay={{ 
+          delay: 3000,
+        }}
+      >
+        <SwiperSlide><img className='min-w-full' src="https://images-eu.ssl-images-amazon.com/images/G/31/IMG20/Home/2024/Gateway/ATFGW/MHS_GW_Candle-holders_PC-under._CB560634205_.jpg" alt="" /></SwiperSlide>
+        <SwiperSlide><img className='min-w-full' src="https://images-eu.ssl-images-amazon.com/images/G/31/img24/Consumables/GW/Unrec/Pets-PC_GW_Hero_3000x1200_01._CB578900116_.jpg" alt="" /></SwiperSlide>
+        <SwiperSlide><img className='min-w-full' src="https://images-eu.ssl-images-amazon.com/images/G/31/IMG24/Smart_Watches/3000X1200_New_Launch_March_hero1._CB580055456_.jpg" alt="" /></SwiperSlide>
+        <SwiperSlide><img className='min-w-full' src="https://images-eu.ssl-images-amazon.com/images/G/31/OHL/24/BAU/feb/PC_hero_1_2x_1._CB582889946_.jpg" alt="" /></SwiperSlide>
+        ...
+      </Swiper></div>
   )
 }
 
