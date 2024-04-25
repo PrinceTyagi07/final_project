9119e25f3bd94a51b85d5abba0543cb6a4495ebe
Prnce Tyagi <princetyagi1901@gmail.com>
2024-04-25 12:28:14 +0530

    new

--- src/Pages/Men.jsx
+++ src/Pages/Men.jsx
@@ -27,10 +27,10 @@ const Men = ({setChanges}) => {
           delay: 3000,
         }}
       >
-        <SwiperSlide><img className='min-w-full' src="https://images-eu.ssl-images-amazon.com/images/G/31/IMG20/Home/2024/Gateway/ATFGW/MHS_GW_Candle-holders_PC-under._CB560634205_.jpg" alt="" /></SwiperSlide>
-        <SwiperSlide><img className='min-w-full' src="https://images-eu.ssl-images-amazon.com/images/G/31/img24/Consumables/GW/Unrec/Pets-PC_GW_Hero_3000x1200_01._CB578900116_.jpg" alt="" /></SwiperSlide>
-        <SwiperSlide><img className='min-w-full' src="https://images-eu.ssl-images-amazon.com/images/G/31/IMG24/Smart_Watches/3000X1200_New_Launch_March_hero1._CB580055456_.jpg" alt="" /></SwiperSlide>
-        <SwiperSlide><img className='min-w-full' src="https://images-eu.ssl-images-amazon.com/images/G/31/OHL/24/BAU/feb/PC_hero_1_2x_1._CB582889946_.jpg" alt="" /></SwiperSlide>
+        <SwiperSlide><img className='min-w-full' src="https://m.media-amazon.com/images/S/al-eu-726f4d26-7fdb/a7f174fd-8941-4a6b-9291-d95b4d0d1fe2._CR0%2C0%2C3000%2C600_SX1920_.jpg" alt="" /></SwiperSlide>
+        <SwiperSlide><img className='min-w-full' src="https://m.media-amazon.com/images/S/al-eu-726f4d26-7fdb/3b0236eb-e081-4f48-8aa9-38a6973aec55._CR0%2C0%2C3000%2C600_SX1920_.jpg" alt="" /></SwiperSlide>
+        <SwiperSlide><img className='min-w-full' src="https://m.media-amazon.com/images/S/al-eu-726f4d26-7fdb/29efca34-ec0e-4a21-9204-f4409cd09cf7._CR0%2C0%2C3000%2C600_SX1920_.jpg" alt="" /></SwiperSlide>
+        <SwiperSlide><img className='min-w-full' src="https://m.media-amazon.com/images/S/al-eu-726f4d26-7fdb/4c29ed65-57bf-44a4-b849-8bfb2a995cc7._CR0%2C0%2C3000%2C600_SX1920_.jpg" alt="" /></SwiperSlide>
         ...
       </Swiper></div>
   )
